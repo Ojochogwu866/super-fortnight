4093648975b76ada33fdc74819551dff74fd5a4d
Ojochogwu Dickson <07afrik@gmail.com>
2026-02-19 14:09:58 +0100

    initial commit

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://super-fortnight-be.onrender.com/api';
 let currentUser = null;
 let feedbackSDK = null;
 let messengerWidget = null;
